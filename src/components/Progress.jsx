89dfcf12b8eb02bb09d1d02e61080de469f95c66
Kestutis Kasiulynas <k.kasiulynas@gmail.com>
2018-01-28 02:29:51 +0000

    Set default values for CircularProgress

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -3,7 +3,7 @@ import CircularProgress from 'material-ui/CircularProgress'
 
 const Progress = () => (
   <div style={{ textAlign: 'center', marginTop: '30px' }}>
-    <CircularProgress size={80} thickness={5} />
+    <CircularProgress size={80} thickness={5} mode={'indeterminate'} value={0} min={0} max={100} />
   </div>
 )
 
