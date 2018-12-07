44cb39a23fffecbf071719a6fbce51ce35426130
Kestutis Kasiulynas <k.kasiulynas@gmail.com>
2018-12-07 23:22:45 +0000

    Clean up reducers

--- src/routes/Live/modules/liveReducer.js
+++ src/routes/Live/modules/liveReducer.js
@@ -1,31 +1,24 @@
 // @flow
-import { LIVE_MATCH_DETAILS } from '../../../actions/api'
+import { LIVE_MATCH_DETAILS, MATCH_FINISHED } from '../../../actions/api'
 import { mapAccountToPlayer } from '../../../actions/matchProcessing'
 
 // ------------------------------------
 // Action Handlers
 // ------------------------------------
 const ACTION_HANDLERS = {
-  LIVE_MATCH_DETAILS : (state, action) => {
-    action.payload.teams.forEach((team) => {
-      team.players.map(player => mapAccountToPlayer(player))
-    })
+  [LIVE_MATCH_DETAILS] : (state, action) => {
+    action.payload.teams.forEach(team => team.players.map(mapAccountToPlayer))
     const oldLive = Object.assign({}, state.live, { ...action.payload })
     oldLive.updated = Date.now()
     return Object.assign({}, state, { live: oldLive })
   },
-  MATCH_FINISHED : (state, action) => {
-    return Object.assign({}, state, { matches: [], live: null })
-  }
+  [MATCH_FINISHED] : state => Object.assign({}, state, { matches: [], live: null })
 }
 
 // ------------------------------------
 // Reducer
 // ------------------------------------
-const initialState = {
-  live: {},
-}
-export default function liveReducer(state: { counter: number, matches: [] } = initialState, action) {
+export default function liveReducer(state: any = { live: {} }, action: any) {
   const handler = ACTION_HANDLERS[action.type]
   return handler ? handler(state, action) : state
 }
