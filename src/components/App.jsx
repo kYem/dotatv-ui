5239cdca951e4552e250b44e0253e62b62e7b2f0
Kestutis Kasiulynas <k.kasiulynas@gmail.com>
2018-12-31 04:09:41 +0000

    Use bootstrap navbar for navigation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,8 +9,8 @@ import '../styles/main.scss'
 const store = createStore(window.__INITIAL_STATE__)
 
 if (process.env.NODE_ENV !== 'production') {
-  const {whyDidYouUpdate} = require('why-did-you-update');
-  whyDidYouUpdate(React, { include: [/^PlayerTable/]})
+  const { whyDidYouUpdate } = require('why-did-you-update')
+  whyDidYouUpdate(React, { include: [/^PlayerTable/] })
 }
 
 class App extends React.Component {
@@ -25,15 +25,30 @@ class App extends React.Component {
         <React.StrictMode>
           <div style={{ height: '100%' }}>
             <Router>
-              <div className='container main-container'>
-                <div className='navigation'>
-                  <h1>Dota Tv</h1>
-                  <NavLink to='/' activeClassName='page-layout__nav-item--active'>Home</NavLink>
+
+              <nav className='navbar navbar-expand navbar-dark bg-dark'>
+                <div className='container'>
+                  <div className='logo-name-container navbar-brand'>
+                    <h1>Dota Tv</h1>
+                  </div>
+                  <div className='collapse navbar-collapse' id='navbarTogglerDemo02'>
+                    <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
+                      <li className='nav-item active'>
+                        <NavLink to='/' className='nav-link' activeClassName='active'>
+                          Home
+                        </NavLink>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
+              </nav>
+
+              <div className='container main-container'>
                 <div className='page-layout__viewport'>
                   <Route path='/' component={HomeView} />
                 </div>
               </div>
+
             </Router>
           </div>
         </React.StrictMode>
