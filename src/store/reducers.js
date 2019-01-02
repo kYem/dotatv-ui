be9ef478d11538251153c83c0aa574c13576de30
Kestutis Kasiulynas <k.kasiulynas@gmail.com>
2019-01-02 18:10:16 +0000

    Remove live reducer, not being used atm

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux'
 import home from '../routes/Home/modules/homeReducer'
-import live from '../routes/Live/modules/liveReducer'
 import streamReducer from '../routes/streams/streamReducer'
 
 const makeRootReducer = asyncReducers => combineReducers({
   home,
-  live,
   streams: streamReducer,
   ...asyncReducers
 })
